2219031f3325cc3dd09a400ecd8b1f773cbd39b9
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2015-03-07 01:02:18 -0600

    Lab4

---
+++ int.asm
@@ -0,0 +1,17 @@
+	.ORIG x1200
+	ADD R6, R6, #-2
+	STW R0, R6, #0
+	ADD R6, R6, #-2
+	STW R1, R6, #0 
+	LEA R0, ADDR
+	LDW R0, R0, #0; R0 = x4000
+	LDW R1, R0, #0
+	ADD R1, R1, #1
+	STW R1, R0, #0	
+	LDW R1, R6, #0
+	ADD R6, R6, #2
+	LDW R0, R6, #0
+	ADD R6, R6, #2
+	RTI
+ADDR 	.FILL x4000
+	.end	
